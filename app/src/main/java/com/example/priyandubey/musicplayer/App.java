360521b60450e1711f28168025f4c240d17f0c79
priyandubey <Priyanshudubey74@gmail.com>
2019-09-02 10:44:01 +0530

    Added Notification Channel

---
+++ app/src/main/java/com/example/priyandubey/musicplayer/App.java
@@ -0,0 +1,38 @@
+package com.example.priyandubey.musicplayer;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class App extends Application {
+
+    public static final String CHANNEL_ID = "playerNotification";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotification();
+    }
+
+    public void createNotification() {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+        NotificationChannel channel = new NotificationChannel(
+                CHANNEL_ID,
+                "musicNotify",
+                NotificationManager.IMPORTANCE_DEFAULT
+        );
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
+    }
+
+    }
+
+
+}
